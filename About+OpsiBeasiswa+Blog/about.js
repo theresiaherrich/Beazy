9fff0f6654b959badd3c8a1714e4b32e804d525a
theresiaherrich <124873369+theresiaherrich@users.noreply.github.com>
2025-06-07 19:09:34 +0700

    menyesuaikan dan merevisi website serta menyambungkan ke satu sama lain page

--- About+OpsiBeasiswa+Blog/about.js
+++
@@ -1,8 +0,0 @@
-
-    const menuToggle = document.getElementById("menu-toggle");
-    const navMenu = document.getElementById("nav-menu");
-
-    menuToggle.addEventListener("click", () => {
-        navMenu.classList.toggle("show");
-    });
-
